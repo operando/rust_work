088c3d381025e108de1ffdcd44cfe8ea1caa32c9
Shinobu Okano <okanoshinobu8@gmail.com>
2019-02-15 13:21:19 +0900

    Update sample

--- first_sample/src/main.rs
+++ first_sample/src/main.rs
@@ -1,4 +1,26 @@
 fn main() {
     println!("Hello, world!");
     println!("{}", 1 + 3);
+//    loop {
+//        println!("Hello, world!");
+//    }
+
+    // ループ
+    // https://doc.rust-jp.rs/the-rust-programming-language-ja/1.6/book/loops.html
+
+    let mut i = 0;
+    while i < 5 {
+        i += 1;
+        println!("{}", i);
+    }
+
+    // output : 0 1 2 3 4
+    for i in 0..5 {
+        println!("{}", i);
+    }
+
+    // iにindexが入る
+    for (i, j) in (5..10).enumerate() {
+        println!("i = {} and j = {}", i, j);
+    }
 }
